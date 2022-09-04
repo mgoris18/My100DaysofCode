ad78b9ea9c27368745fdf2b2c3ae5e6031304a63
Michell <72279305+mgoris18@users.noreply.github.com>
2022-09-04 14:56:29 -0400

    Update Day One Code- Automated-Decision Making (anime).py

--- Day One/Day One Code- Automated-Decision Making (anime).py
+++ Day One/Day One Code- Automated-Decision Making (anime).py
@@ -1,1 +1,15 @@
+from random import choice
 
+# create lists of animes
+animes = [['Haikyuu', 'Sports', 'Short', 'Series'],
+['Naruto', 'adventure', 'forever', 'Series'],
+['Jujutsu Kaisen', 'Adventure', 'Short', 'Series'], 
+['Bungo Stray Dogs', 'Action', 'Long', 'Series']]
+
+# Input mood
+print('What mood are you in?')
+mood = input()
+
+for item in animes:
+    if item[1] == mood:
+       print (mood + 'anime: ' + item[0])
